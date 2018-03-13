b9352ac4eb9805e2cd1e67bbc3a5b631ec305371
Кривой Иван <IKrivoy.SBT@sberbank.ru>
2018-03-13 20:49:56 +0300

    333

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeLab.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeLab.java
@@ -2,12 +2,17 @@ package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by admin on 12.03.2018.
  */
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
@@ -15,5 +20,24 @@ public class CrimeLab {
         return sCrimeLab;
     }
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Для каждого второго объекта
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 }
